321dcf6f891196233f3ee3df9967fff4a5d4d24e
akhanal07 <7030ak58@gmail.com>
2024-06-12 16:25:44 +0530

    React

---
+++ my-app5/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Greet';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+     
+      <h1>HELLO WORLD</h1>
+        
+        <h1>HELLO</h1>
+
+        <Greet></Greet>
+        <Greet></Greet>
+        <Greet></Greet>
+        <Greet></Greet>
+        <Greet></Greet>
+        <Greet></Greet>
+        <Greet></Greet>
+        
+
+        
+      
+    </div>
+  );
+}
+
+export default App;
