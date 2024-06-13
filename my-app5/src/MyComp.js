c39c0e4cebdbea0f694839179566ca58f9f04366
akhanal07 <7030ak58@gmail.com>
2024-06-13 12:40:58 +0530

    React

--- my-app5/src/MyComp.js
+++ my-app5/src/MyComp.js
@@ -3,15 +3,15 @@ import React from 'react'
 function MyComp() {
     const[name,setName]=React.useState("Ayush");
 
-    function hi(){
-        setName("Khanal")
-    }
+    // function hi(){
+    //     setName("Khanal")
+    // }
 
   return (<>
     <div>Click the below button</div>
     <br></br>
 
-    <button onClick={()=> hi()}>Submit</button>
+    <button onClick={()=> setName("Khanal")}>Submit</button>
     <h1>{name}</h1>
     </>
   )
