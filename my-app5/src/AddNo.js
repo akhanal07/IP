40c2ac75a18626427da4b33a4a477d2b2b7f88a8
akhanal07 <7030ak58@gmail.com>
2024-06-13 15:19:49 +0530

    React

--- my-app5/src/AddNo.js
+++ my-app5/src/AddNo.js
@@ -4,11 +4,17 @@ function AddNo() {
     const [num1, SetNum1] = React.useState(0);
     const [num2, SetNum2] = React.useState(0);
     const [total, SetTotal] = React.useState(0); 
+    const [total1, SetTotal1] = React.useState(0);
     
 
     function sum() {
         const total = parseInt(num1) + parseInt(num2);
         SetTotal(total); 
+      }
+
+      function multiply() {
+        const total1 = parseInt(num1) * parseInt(num2);
+        SetTotal1(total1); 
       }
       
       return (
@@ -16,8 +22,10 @@ function AddNo() {
           <div>
             <input onChange={(e) => SetNum1(e.target.value)} type='num' placeholder='Write the first no'></input><br></br>
             <input onChange={(e) => SetNum2(e.target.value)} type='text' placeholder='Write the second no'></input><br></br>
-            <button onClick={sum}>Add</button>
+            <button onClick={sum}>sum</button>
+            <button onClick={multiply}>multiply</button>
             <h3>The sum is: {total}</h3>
+            <h3>The multiplication is: {total1}</h3>
           </div>
         </>
       )
