15061c648f6506824747b681cec057ba9b6ad5f7
akhanal07 <7030ak58@gmail.com>
2024-06-13 15:24:29 +0530

    React

--- my-app5/src/AddNo.js
+++ my-app5/src/AddNo.js
@@ -5,6 +5,7 @@ function AddNo() {
     const [num2, SetNum2] = React.useState(0);
     const [total, SetTotal] = React.useState(0); 
     const [total1, SetTotal1] = React.useState(0);
+    const [total2, SetTotal2] = React.useState(0);
     
 
     function sum() {
@@ -16,6 +17,11 @@ function AddNo() {
         const total1 = parseInt(num1) * parseInt(num2);
         SetTotal1(total1); 
       }
+
+      function diff() {
+        const total2 = parseInt(num1) - parseInt(num2);
+        SetTotal2(total2); 
+      }
       
       return (
         <>
@@ -24,8 +30,11 @@ function AddNo() {
             <input onChange={(e) => SetNum2(e.target.value)} type='text' placeholder='Write the second no'></input><br></br>
             <button onClick={sum}>sum</button>
             <button onClick={multiply}>multiply</button>
+            <button onClick={diff}>Diference</button>
             <h3>The sum is: {total}</h3>
             <h3>The multiplication is: {total1}</h3>
+            <h3>The difference  is: {total2}</h3>
+
           </div>
         </>
       )
