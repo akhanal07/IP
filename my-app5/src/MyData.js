d33bd8390d6ab9150b4417bf3702a30589f0de93
akhanal07 <7030ak58@gmail.com>
2024-06-14 12:58:18 +0530

    React

--- my-app5/src/MyData.js
+++ my-app5/src/MyData.js
@@ -1,14 +1,22 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function MyData() {
-  const [data, setData] = React.useState([]);
-  const myFunc = async () => {
-    const response = await axios.get("https://reqres.in/api/users?page=2");
-    
-    setData(response.data.data);
-  };
-  myFunc();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://reqres.in/api/users?page=2");
+        setData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <table border="1cm">
@@ -21,20 +29,18 @@ function MyData() {
           </tr>
         </thead>
         <tbody>
-          {data.map((user, key) => {
-            return (
-              <tr key={key}>
-                <td>{user.id}</td>
-                <td>
-                  {user.first_name} {user.last_name}
-                </td>
-                <td>{user.email}</td>
-                <td>
-                  <img src={user.avatar} alt="user avatar" />
-                </td>
-              </tr>
-            );
-          })}
+          {data.map((user, key) => (
+            <tr key={key}>
+              <td>{user.id}</td>
+              <td>
+                {user.first_name} {user.last_name}
+              </td>
+              <td>{user.email}</td>
+              <td>
+                <img src={user.avatar} alt="user avatar" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
