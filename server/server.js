cceab66fcf34b1e949b16f9e70335f7ab84314ae
akhanal07 <7030ak58@gmail.com>
2024-08-16 12:32:52 +0530

    IP

--- server/server.js
+++ server/server.js
@@ -1,13 +1,46 @@
-const http = require('http');
-const PORT = 8080;
+// GET Request (Read)
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => console.log('GET Request Successful:', data))
+  .catch(error => console.error('There was a problem with the GET request:', error));
 
-const server = http.createServer((req, res) => {
+// POST Request (Create)
+const postData = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1
+};
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(postData)
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => console.log('POST Request Successful:', data))
+  .catch(error => console.error('There was a problem with the POST request:', error));
 
-    res.end('Hello, World!\n');
-});
-
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
+// DELETE Request (Delete)
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE'
+})
+  .then(response => {
+    if (response.ok) {
+      console.log('DELETE Request Successful');
+    } else {
+      console.error('There was a problem with the DELETE request:', response.statusText);
+    }
+  })
+  .catch(error => console.error('There was a problem with the DELETE request:', error));
